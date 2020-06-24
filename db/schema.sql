6b57b468756c463b4fa25fc5438d756bece204f2
Demetrio <demetrioalmeida@gmail.com>
2020-06-23 23:36:23 -0400

    homework

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,10 @@
 CREATE DATABASE burgers_db;
 
 use burgers_db;
+
+CREATE TABLE  burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("chicken breast", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("steak burger", false);
