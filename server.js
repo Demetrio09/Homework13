03b3eddc0aee58b86e6c99c89be6dbda0fbc83ab
Demetrio <demetrioalmeida@gmail.com>
2020-06-24 23:29:41 -0400

    homework import routes to server file

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require("express");
 
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
